282cc6901d845626b0a95bd052750602c155b061
reza <mardanireza58@gmail.com>
2025-01-21 10:58:22 +0330

    modify: ```Home view and home template for slider

--- home_app/views.py
+++ home_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from blog_app.models import Article
 # Create your views here.
 
 def home_page(request):
-    return render(request, 'home_app/index.html', {})
+    articles = Article.objects.all()
+    return render(request, 'home_app/index.html', context={'articles':articles})
