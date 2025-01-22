b253c17cebb1f1a5031abd73b43c908427b00aa1
reza <mardanireza58@gmail.com>
2025-01-22 10:42:17 +0330

    add: ```category model for articles```

--- blog_app/models.py
+++ blog_app/models.py
@@ -2,9 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+
 class Article(models.Model):
-    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
     title = models.CharField(max_length=70, help_text='enter title for post', unique=True)
     body  = models.TextField()
     image = models.ImageField(upload_to='images/articles')
