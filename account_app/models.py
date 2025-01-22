b12193c5eb0016201dafbf0aa22571aae0a61fca
reza <mardanireza58@gmail.com>
2025-01-22 13:19:51 +0330

    add: ```profile models for users```

--- account_app/models.py
+++ account_app/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    father_name = models.CharField(max_length=25)
+    melli_code = models.CharField(max_length=10)
+    image = models.ImageField(upload_to="profiles/images")
+    
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.melli_code}" 
