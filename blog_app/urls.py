abda3ddb3d23e653405dc9edf69b7889c650a556
reza <mardanireza58@gmail.com>
2025-01-26 16:40:54 +0330

    add:; ```post detail page and view```

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
-    
+    path('post/<str:id>', views.post_detail, name='post-detail')
 ]
--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home_app.urls')),
     path('account/', include('account_app.urls')),
-    path('blog', include('blog_app.urls'))
+    path('blog/', include('blog_app.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
