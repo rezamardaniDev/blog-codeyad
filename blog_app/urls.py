a7f8fae3a3b31eb20fbfbe5f75183adf9e0b0e0a
reza <mardanireza58@gmail.com>
2025-01-31 16:22:34 +0330

    modify: ```header and footer section cuted and move to template folder

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('post/<slug:slug>', views.post_detail, name='post-detail')
+    path('post/<slug:slug>', views.post_detail, name='post-detail'),
+    path('all', views.all_posts, name='all-post')
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -7,3 +7,7 @@ def post_detail(request, slug):
     article = get_object_or_404(Article, slug=slug)
     print(reverse('blog:post-detail', args=[article.slug]))
     return render(request, 'blog_app/post_detail.html', context={'article':article})
+
+def all_posts(request):
+    articles = Article.objects.all()
+    return render(request, 'blog_app/posts_list.html', context={'articles':articles})
